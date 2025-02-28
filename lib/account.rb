4daec45437dd0cbe6bf46b5e902098eed9c13f98
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-02-28 15:55:57 +0500

    Account expiry and error if no account owner is set ( done )

---
+++ lib/account.rb
@@ -0,0 +1,30 @@
+class Account
+  STANDARD_VALIDITY_YRS = 5
+  attr_reader :account_status, :owner 
+  def initialize(attrs = {})
+  @account_status = :active
+    set_owner(attrs[:owner]) 
+  end
+
+  def set_expire_date
+  Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  def deactivate
+    @account_status = :deactivated
+  end 
+  private 
+  def set_owner(obj)
+    obj.nil? ? missing_owner : @owner = obj
+  end
+  def missing_owner
+    raise 'An Account owner is required'
+  end
+end
+
+# Instance method will shut down (deactivate) the single account but the class method will deactivate all accounts there .
+
+# (Do you really need Account::STANDARD_VALIDITY_YRS? Perhaps STANDARD_VALIDITY_YRS in enough? Try it out...)
+
+# We don't need to use Account::STANDARD_VALIDITY_YRS, we can use STANDARD_VALIDITY_YRS instead.
+# because we are not calling it from another class. We can use it directly.
---
+++ spec/account_spec.rb
@@ -0,0 +1,45 @@
+require "./lib/account.rb"
+require "date"
+
+describe Account do
+  let(:account){ Account.new(owner: person)}
+
+  let(:person){instance_double('Person', name: 'Fahad')}
+  subject{described_class.new({owner: person})}
+
+  it "consist of 4 digit pin code" do 
+    number = rand(1000..9999)
+    number_length = Math.log10(number).to_i + 1
+    expect(number.digits.count).to eq 4
+  end
+
+  it "needs an expiry date on initialize ( 5years )" do
+    expected_date = (Date.today.next_year(5)).strftime('%m/%y')
+    expect(account.set_expire_date).to eq expected_date
+  end
+
+  it "is expected to have :active status on initialize" do
+  expect(subject.account_status).to eq :active
+  end
+
+  it "deactivates account using instance method" do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
+  end
+
+  it "is expected to have an owner" do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
+  end
+  
+
+
+  # Used for deactiving multiple accounts
+  # it "deactivates account using class method" do
+  #   Account.deactivated(subject)
+  #   expect(subject.account_status).to eq :deactivated
+  # end
+end
